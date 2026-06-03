a4876423a06d606dfe7c6f1b24e941781bf86c14
Uniba-Muzammil <unibamuzammil8@gmail.com>
2026-06-04 03:08:02 +0500

    updated for frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api", // backend
-  withCredentials: true,                 // VERY IMPORTANT for cookies
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+  withCredentials: true,
 });
-
 
 // Auth
 export const registerUser = (data) =>
